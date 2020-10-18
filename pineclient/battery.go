2b5d63e2da7d9ad20f026e3a4bec7484f8ffbeb2
nolim1t <hello@nolim1t.co>
2020-10-18 16:09:36 +0700

    Add comments

--- pineclient/battery.go
+++ pineclient/battery.go
@@ -1,12 +1,22 @@
-package pineclient
+package pineclient // Define package name
+/*
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
 
+// Define imports
 import (
     "fmt"
     "io/ioutil"
     "strings"
 )
 
-// Some sample pathnames (this pertains to my pinephone)
+// Define Constants (Accessible from outside this package)
 const (
     BatteryPathName = "/sys/class/power_supply/axp20x-battery"
     BatteryIsPresent = BatteryPathName + "/present"
@@ -23,10 +33,12 @@ func stringtofile(filename string) string {
 }
 
 // Static Methods
+// Status of Battery (-1 if file not present, 0 if battery not present, 1 if battery is present)
 func GetStatus() (string) {
     return stringtofile(BatteryIsPresent)
 }
 
+// Capacity of Battery (-1 if file not present, or percentage)
 func GetCapacity() (string) {
     return fmt.Sprintf("%s", stringtofile(BatteryCapacity))
 }
