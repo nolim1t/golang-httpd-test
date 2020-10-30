7881f754862319d78275bf7f8de340db925cdd83
nolim1t <hello@nolim1t.co>
2020-10-30 11:05:19 +0700

    Add default value for host and AuthScheme (none)

--- common/config.go
+++ common/config.go
@@ -17,12 +17,13 @@ type (
 		BitcoinClient           bool   `toml:"bitcoin-client"`
 		// [bitcoind] section in the `--config` file that defines Bitcoind's setup
 		Bitcoind Bitcoind `toml:"bitcoind"`
+		// auth-scheme key
+		AuthScheme string `toml:"auth-scheme" default:"none"` // either use omitempty or default (https://godoc.org/github.com/pelletier/go-toml)
 	}
-
 	// Bitcoind config
 	// NOTE: Keep in mind that this is **not yet encrypted**, so best to keep it _local_
 	Bitcoind struct {
-		Host string `toml:"host"`
+		Host string `toml:"host" default:"localhost"`
 		Port int64  `toml:"port"`
 		User string `toml:"user"`
 		Pass string `toml:"pass"`
